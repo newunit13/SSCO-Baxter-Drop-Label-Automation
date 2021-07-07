a4c5e5ffd2eb59a01c224abcd2016ac1208f14e5
Edward Russell <newunit13@gmail.com>
2021-07-07 14:48:17 -0400

    Added application icon

--- app.py
+++ app.py
@@ -156,6 +156,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('hand_icon.ico'))
     win = MainWindow()
     win.show()
     sys.exit(app.exec_())
